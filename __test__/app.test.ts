de6c181ed25f238761a5bf684d7f062269991e5a
DelPozoAmoAndres <asdfandres09@gmail.com>
2024-05-03 17:35:32 +0200

    Quitamos CORS provisionalmente

--- __test__/app.test.ts
+++ __test__/app.test.ts
@@ -1,29 +1,29 @@
 import request from 'supertest';
 import app from '../app';
-describe('CORS', () => {
-    describe('when the origin is allowed', () => {
-        test('should allow requests from allowed origins', async () => {
-            const response = await request(app)
-                .get('/')
-                .set('Origin', 'https://astour.online');
+// describe('CORS', () => {
+//     describe('when the origin is allowed', () => {
+//         test('should allow requests from allowed origins', async () => {
+//             const response = await request(app)
+//                 .get('/')
+//                 .set('Origin', 'https://astour.online');
 
-            expect(response.status).toBe(200);
-        });
-    });
-    describe('when the origin is not allowed', () => {
-        test('should reject requests from disallowed origins', async () => {
-            const response = await request(app)
-                .get('/')
-                .set('Origin', 'https://example.com');
+//             expect(response.status).toBe(200);
+//         });
+//     });
+//     describe('when the origin is not allowed', () => {
+//         test('should reject requests from disallowed origins', async () => {
+//             const response = await request(app)
+//                 .get('/')
+//                 .set('Origin', 'https://example.com');
 
-            expect(response.status).toBe(500);
-        });
+//             expect(response.status).toBe(500);
+//         });
 
-        test('should reject requests without an origin', async () => {
-            const response = await request(app)
-                .get('/');
+//         test('should reject requests without an origin', async () => {
+//             const response = await request(app)
+//                 .get('/');
                 
-            expect(response.status).toBe(500);
-        });
-    });
-});
+//             expect(response.status).toBe(500);
+//         });
+//     });
+// });
